18c45c4bdf2fba8b3dfc5e12271da99101b2806e
Filipe Oliveira <108694890+Fa0s@users.noreply.github.com>
2023-07-24 13:54:41 -0300

    Implementação Swipper
    Swipper implementado e testado

--- assets/js/index.js
+++ assets/js/index.js
@@ -25,3 +25,27 @@ btn3.addEventListener('click', () => {
 btn4.addEventListener('click', () => {
     menu.classList.remove('menu_lateral-ativo')
     });
+
+    
+    const swiper = new Swiper('.swiper', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+      
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      
+        // And if we need scrollbar
+        scrollbar: {
+          el: '.swiper-scrollbar',
+        },
+      });
